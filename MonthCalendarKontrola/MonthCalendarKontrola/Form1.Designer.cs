fa8dcb3cabcee67a05c939df59541ab4e4cfcd4c
DavidTrstenjak <90246263+DavidTrstenjak@users.noreply.github.com>
2022-01-18 13:07:32 +0100

    Add files via upload

---
+++ MonthCalendarKontrola/MonthCalendarKontrola/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace MonthCalendarKontrola
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.monCal = new System.Windows.Forms.MonthCalendar();
+            this.btnTermin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // monCal
+            // 
+            this.monCal.Location = new System.Drawing.Point(18, 18);
+            this.monCal.MaxSelectionCount = 14;
+            this.monCal.Name = "monCal";
+            this.monCal.TabIndex = 0;
+            // 
+            // btnTermin
+            // 
+            this.btnTermin.Location = new System.Drawing.Point(18, 202);
+            this.btnTermin.Name = "btnTermin";
+            this.btnTermin.Size = new System.Drawing.Size(199, 53);
+            this.btnTermin.TabIndex = 1;
+            this.btnTermin.Text = "Odaberite termin";
+            this.btnTermin.UseVisualStyleBackColor = true;
+            this.btnTermin.Click += new System.EventHandler(this.btnTermin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(235, 284);
+            this.Controls.Add(this.btnTermin);
+            this.Controls.Add(this.monCal);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar monCal;
+        private System.Windows.Forms.Button btnTermin;
+    }
+}
+
---
+++ MonthCalendarKontrola/MonthCalendarKontrola/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MonthCalendarKontrola
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTermin_Click(object sender, EventArgs e)
+        {
+            TimeSpan period = monCal.SelectionEnd - monCal.SelectionStart;
+
+            if (period.Days < 1)
+            {
+                MessageBox.Show("Pritiskom na tipku SHIFT morate odabrati broj dana " , "GREŠKA", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }else
+
+            MessageBox.Show("Rezervirali ste period od " + monCal.SelectionStart.ToShortDateString() + " do " + monCal.SelectionEnd.ToShortDateString(), "Rezervacija" , MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
